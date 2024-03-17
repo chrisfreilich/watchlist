5ee33080a14b4c451a68f8849ecacb1c18f60f04
chrisfreilich <chrisfreilich@gmail.com>
2024-03-17 17:28:04 -0400

    Films render from the API on the search screen

--- index.js
+++ index.js
@@ -8,23 +8,67 @@ searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight/2)
 window.addEventListener('resize', ()=>searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight/2))
 
 // Search
-// searchBtn.addEventListener('click', doSearch)
 searchBarEl.addEventListener('submit', (e) => {
     e.preventDefault()
-    fetch(`http://www.omdbapi.com/?s=${searchTitleEl.value.trim()}&apikey=88ae5a67`)
-    .then( res => res.json() )
-    .then( data => processSearchResults(data))
+    const searchString = searchTitleEl.value.trim()
+    if (!searchString) return
+    fetch(`http://www.omdbapi.com/?s=${searchString}&apikey=88ae5a67`)
+        .then( res => res.json() )
+        .then( data => processSearchResults(data))
 })
 
 function processSearchResults(data) {
     let html = ""
+    let filmIDs = []
     for ( const film of data.Search) {
-        html += `<div class='film-card'>
-
-        ${film.Title}
-        
-        
-        </div>`
+        // In this function, add the elements, but we're going to need to do
+        // another API for each movie to get the details of the film (kind of weird design of the API, methinks!)
+        html += `<div class='film-card' id=${film.imdbID}></div><hr>`
+        filmIDs.push(film.imdbID)
     }
     document.getElementById('search-results').innerHTML = html
+
+    // Now that we have the elements in place, we can call the API to fill in the data
+    for (const id of filmIDs) {
+        fetch(`https://www.omdbapi.com/?i=${id}&plot=full&apikey=88ae5a67`)
+        .then( res => res.json() )
+        .then( data => {
+            // Calculate Rotten Tomatoes stuff
+            let rtRating = data.Ratings.find(rating => rating.Source === "Rotten Tomatoes")
+            rtRating = rtRating ? rtRating.Value : ""
+            let rtIcon
+            let rtRatingAlt
+            if (!rtRating || rtRating === "N/A") {
+                rtIcon = "images/tomatometer-empty.svg"
+                rtRating = ""
+                rtRatingAlt = "No Rotten Tomatoes Rating"
+            } else if (parseInt(rtRating, 10) < 60) {
+                rtIcon = "images/tomatometer-rotten.svg"
+                rtRatingAlt = "Rotten Tomatoes Rotten Rating"
+            } else {
+                rtIcon = "images/tomatometer-fresh.svg"
+                rtRatingAlt = "Rotten Tomatoes Fresh Rating"
+            }
+
+            document.getElementById(id).innerHTML = `
+                    <img class="poster" src="${data.Poster === 'N/A' ? 'images/no-poster.jpg' : data.Poster}" alt="">
+                    <div class="film-title-block">
+                        <h4>${data.Title}</h4>
+                        <img src="${rtIcon}" alt="${rtRatingAlt}" />
+                        <p>${rtRating}</p>
+                    </div>
+                    <div class="film-info-block">
+                        <p class="runtime">${data.Runtime}</p>
+                        <p class="genre">${data.Genre}</p>
+                        <img src="/images/plus-icon.png" alt="Button to Add ${data.Title} to Watchlist" />
+                        <p class="watchlist-btn">Watchlist</p>
+                    </div>
+                    <div class="film-plot-block">
+                        <span>${data.Plot === "N/A" ? "No description available." : data.Plot}</span>
+                        <a href="#">(show more)</a>
+                    </div>           
+            `
+        })
+    }
+
 }
