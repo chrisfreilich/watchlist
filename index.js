6d025d3af98a6d9747d468b8eed07c49fa693b06
chrisfreilich <chrisfreilich@gmail.com>
2024-03-18 23:08:35 -0400

    Responsive to tiny screens

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const paginatorEl = document.getElementById('paginator')
 const pgNextBtn = document.getElementById('paginator-next')
 const pgBackBtn = document.getElementById('paginator-back')
 const pgPageEls = document.getElementsByClassName('paginator-page')
-let pgResultPages = 0
+//let pgResultPages = 0
 
 //--- Place the search bar (placement is down to the pixel of screen size, so no media query). ---//
-searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight/2)
+searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight / 2)
 
 //--- Event handlers ---//
 window.addEventListener('resize', handleWindowResize)   // UI responds to any resize
 searchBarEl.addEventListener('submit', (e) => {         // Search form submit
     e.preventDefault()
-    doSearch(1, "start")})
+    doSearch(1, "start")
+})
 paginatorEl.addEventListener('click', handlePaginatorClick) // Paginator action
 resultsEl.addEventListener('click', handleResultsClick)     // Add/Remove from Watchlist
 
@@ -26,14 +27,14 @@ function doSearch(page = 1, offsetPage = "none") {
     const searchString = searchTitleEl.value.trim()
     if (!searchString) return
     fetch(`http://www.omdbapi.com/?s=${searchString}&page=${page}&apikey=88ae5a67`)
-        .then( res => res.json() )
-        .then( data => processSearchResults(data, page, offsetPage))
+        .then(res => res.json())
+        .then(data => processSearchResults(data, page, offsetPage))
 }
 
 function processSearchResults(data, page, offsetPage) {
 
     // Scroll to the top
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 
     // Check for no results
     if (!data.Search) {
@@ -59,26 +60,30 @@ function processSearchResults(data, page, offsetPage) {
     // Now that we have the film-card elements in place for each film, we can call the API to fill in the data
     for (const id of filmIDs) {
         fetch(`https://www.omdbapi.com/?i=${id}&plot=full&apikey=88ae5a67`)
-        .then( res => res.json() )
-        .then( data => {
-            // Calculate Rotten Tomatoes stuff
-            let rtRating = data.Ratings.find(rating => rating.Source === "Rotten Tomatoes")
-            rtRating = rtRating ? rtRating.Value : ""
-            let rtIcon
-            let rtRatingAlt
-            if (!rtRating || rtRating === "N/A") {
-                rtIcon = "images/tomatometer-empty.svg"
-                rtRating = ""
-                rtRatingAlt = "No Rotten Tomatoes Rating"
-            } else if (parseInt(rtRating, 10) < 60) {
-                rtIcon = "images/tomatometer-rotten.svg"
-                rtRatingAlt = "Rotten Tomatoes Rotten Rating"
-            } else {
-                rtIcon = "images/tomatometer-fresh.svg"
-                rtRatingAlt = "Rotten Tomatoes Fresh Rating"
-            }
+            .then(res => res.json())
+            .then(data => {
+                // Calculate Rotten Tomatoes and film info
+                let rtRating = data.Ratings.find(rating => rating.Source === "Rotten Tomatoes")
+                rtRating = rtRating ? rtRating.Value : ""
+                let rtIcon
+                let rtRatingAlt
+                let genre = ""
+                let runtime = ""
+                if (!rtRating || rtRating === "N/A") {
+                    rtIcon = "images/tomatometer-empty.svg"
+                    rtRating = ""
+                    rtRatingAlt = "No Rotten Tomatoes Rating"
+                } else if (parseInt(rtRating, 10) < 60) {
+                    rtIcon = "images/tomatometer-rotten.svg"
+                    rtRatingAlt = "Rotten Tomatoes Rotten Rating"
+                } else {
+                    rtIcon = "images/tomatometer-fresh.svg"
+                    rtRatingAlt = "Rotten Tomatoes Fresh Rating"
+                }
+                runtime = data.Runtime === "N/A" ? "" : data.Runtime
+                genre = data.Genre === "N/A" ? "" : data.Genre
 
-            document.getElementById(id).innerHTML = `
+                document.getElementById(id).innerHTML = `
                     <img class="poster" src="${data.Poster === 'N/A' ? 'images/no-poster.jpg' : data.Poster}" alt="">
                     <div class="film-title-block">
                         <h4>${data.Title}</h4>
@@ -88,8 +93,8 @@ function processSearchResults(data, page, offsetPage) {
                         </div>
                     </div>
                     <div class="film-info-block">
-                        <p class="runtime">${data.Runtime}</p>
-                        <p class="genre">${data.Genre}</p>
+                        ${runtime && `<p class="runtime">${runtime}</p>`}
+                        ${genre && `<p class="genre">${genre}</p>`}
                         <div class="watchlist-btn-container" data-id="${id}">    
                             <img src="/images/${localStorage.getItem(id) ? "minus" : "plus"}-icon.png" alt="Button to Add ${data.Title} to Watchlist" />
                             <p class="watchlist-btn">Watchlist</p>
@@ -100,20 +105,20 @@ function processSearchResults(data, page, offsetPage) {
                         <a href="#" class="show-more" data-id="${id}">(show more)</a>
                     </div>           
             `
-            // Set (show more) visibility
-            const plot = document.querySelectorAll(`.film-plot-block[data-id=${id}]`)[0]
-            const link = document.querySelectorAll(`.show-more[data-id=${id}]`)[0]
-            link.addEventListener('click', revealFullPlot)
-            link.style.display = plot.clientHeight < plot.scrollHeight ? "inline" : "none"
-        })
+                // Set (show more) visibility
+                const plot = document.querySelectorAll(`.film-plot-block[data-id=${id}]`)[0]
+                const link = document.querySelectorAll(`.show-more[data-id=${id}]`)[0]
+                link.addEventListener('click', revealFullPlot)
+                link.style.display = plot.clientHeight < plot.scrollHeight ? "inline" : "none"
+            })
     }
 
     // Paginator
     currentResultPages = Math.ceil(parseInt(data.totalResults) / 10)    // 10 films returned per request, so making the pages that length.
-    if (currentResultPages > 1) { 
+    if (currentResultPages > 1) {
         let numPageLinks = Math.min(currentResultPages, 5)            // Display up to 5 page links 
         let currentStartPage = Number(pgPageEls[0].textContent)
-        switch (offsetPage) {    
+        switch (offsetPage) {
             case "start":
                 currentStartPage = 1
                 break
@@ -134,10 +139,10 @@ function processSearchResults(data, page, offsetPage) {
         for (let i = 0; i < 5; i++) {
             const curPage = currentStartPage + i
             pgPageEls[i].textContent = curPage
-            pgPageEls[i].style.display = numPageLinks > i  ? "block" : "none"
+            pgPageEls[i].style.display = numPageLinks > i ? "block" : "none"
             if (page == curPage)
                 pgPageEls[i].classList.add("current")
-            else    
+            else
                 pgPageEls[i].classList.remove("current")
         }
 
@@ -154,7 +159,7 @@ function processSearchResults(data, page, offsetPage) {
 
 }
 
-function updateShowMoreLinks() {    
+function updateShowMoreLinks() {
     for (const plot of document.getElementsByClassName('film-plot-block')) {
         const link = document.querySelectorAll(`.show-more[data-id=${plot.dataset.id}]`)[0]
         link.style.display = plot.clientHeight < plot.scrollHeight ? "inline" : "none"
@@ -162,7 +167,7 @@ function updateShowMoreLinks() {
 }
 
 function handleWindowResize() {
-    searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight/2)
+    searchBarEl.style.top = headerEl.offsetHeight - (searchBarEl.offsetHeight / 2)
     updateShowMoreLinks()
 }
 
@@ -170,7 +175,7 @@ function revealFullPlot(event) {
     event.preventDefault()
     event.target.style.display = "none"
     const card = document.getElementById(event.target.dataset.id)
-    card.style.gridTemplateRows =  "auto auto auto"
+    card.style.gridTemplateRows = "auto auto auto"
 }
 
 function handlePaginatorClick(e) {
